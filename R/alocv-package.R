0ccc145b42a7767ec87cb1cd9a299f2cbdd21408
Francis-Hsu <francishsu91@gmail.com>
2018-09-30 20:19:20 -0400

    Update package Rd file

---
+++ R/alocv-package.R
@@ -0,0 +1,9 @@
+#' @details
+#' Only support for elastic net regression are implemented, for now.
+#' @references 
+#' \itemize{
+#' \item K. R. Rad, A. Maleki, \emph{A scalable estimate of the extra-sample prediction error via approximate leave-one-out}, (2018), \url{https://arxiv.org/abs/1801.10243}.
+#' \item S. Wang, W. Zhou, H. Lu, A. Maleki, and V. Mirrokni, \emph{Approximate Leave-One-Out Approximatet for Fast Parameter Tuning in High Dimensions}, (2018), \url{https://arxiv.org/abs/1807.02694}.
+#' }
+#' @keywords internal
+"_PACKAGE"
